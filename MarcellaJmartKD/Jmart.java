88c8c298af681cf2be6933e75dad7141027d81e7
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-09-11 10:55:18 +0700

    membuat code

--- MarcellaJmartKD/Jmart.java
+++ MarcellaJmartKD/Jmart.java
@@ -1,35 +1,24 @@
 package MarcellaJmartKD;
 
-
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static int  getPromo() {
+        return 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public static String getCustomer() {
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage (int before, int after){
+        if (before > after){
+            float Discount = before-after;
+            return Discount;
+        }
+        else {
+            return 0.0f;
+        }
     }
 }
+    
+    
