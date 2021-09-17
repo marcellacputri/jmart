7da3f80c8a3281eabcee15cddb3a8a249cbf0e4c
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-09-18 02:51:08 +0700

    modul 1 Marcella Cinninthya Putri revisi

--- MarcellaJmartKD/Jmart.java
+++ MarcellaJmartKD/Jmart.java
@@ -1,7 +1,11 @@
-package MarcellaJmartKD;
+package MarcellaJmartKD; 
 
 public class Jmart
 {
+    public static void main (String[] args){
+        
+    }
+    
     public static int  getPromo() {
         return 0;
     }
@@ -11,23 +15,48 @@ public class Jmart
     }
     
     public static float getDiscountPercentage (int before, int after){
+	float discountPercentage;
         if (before > after){
-            float Discount = before-after;
-            return Discount;
+            float discount = before-after;
+            return discountPercentage = (discount / before) *100;
         }
         else {
             return 0.0f;
         }
     }
     
-    public static float getDiscountedPrice (int price, float discountPercentage) {
-        if (discountPercentage > 100.0) {
+    public static int getDiscountedPrice (int price, float discountPercentage) {
+        if (discountPercentage > 100.0f) {
             discountPercentage = 100;
         }
         
-        float totalprice = price - (discountPercentage*price /100);
-        return price;
+        float discountedPrice = price - ((discountPercentage/100) * price);
+        return (int) discountedPrice;
+
     }
+    
+    public static int getOriginalPrice (int discountedPrice, float discountPercentage){       
+        float originalPrice = (discountedPrice * 100)/ (100 - discountPercentage);
+        return (int) originalPrice;
+    }
+    
+    public static float getCommissionMultiplier (){
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice (int price) {
+        float priceFloat = price + (price * getCommissionMultiplier());
+        int priceInt = (int) priceFloat;
+        return priceInt;
+    }
+     
+    public static int getAdminFee (int price){
+        float adminFee = price * getCommissionMultiplier();
+        return (int) adminFee;
+    }
+    
+    
+
 }
     
     
