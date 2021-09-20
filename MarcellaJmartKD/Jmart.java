f14d90bff6b719ab5caa9936f9b2171af75b0de2
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-09-20 21:01:07 +0700

    PT modul2 Marcella Cinninthya Putri

--- MarcellaJmartKD/Jmart.java
+++ MarcellaJmartKD/Jmart.java
@@ -3,13 +3,28 @@ package MarcellaJmartKD;
 public class Jmart
 {
  
-    public static void main(String[] args){
-        System.out.println(create());
-    }
+    //public static void main(String[] args){
+        //System.out.println(create());
+    //}
     
 
-    public static Product create(){
-        return new Product("Dilan", 5, false, new PriceTag(20000L), ProductCategory.BOOK);
+    //public static Product create(){
+        //return new Product("Dilan", 5, false, new PriceTag(20000L), ProductCategory.BOOK);
+    //}
+    
+    public static void main (String[] args){
+    }
+    
+    public static Product createProduct(){
+        return null;
+    }
+    
+    public static Coupon createCoupun(){
+        return null;
+    }
+    
+    public static ShipmentDuration createShipmentDuration(){
+        return null;
     }
 }
         
