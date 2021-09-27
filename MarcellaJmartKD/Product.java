618b0c0bb33ded73fecc1d94a1fcde956a113767
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-09-27 20:58:56 +0700

    PT modul3 MarcellaCP

--- MarcellaJmartKD/Product.java
+++ MarcellaJmartKD/Product.java
@@ -1,40 +1,41 @@
 package MarcellaJmartKD;
 
-public abstract class Product extends Recognizable implements FileParser
+public class Product extends Recognizable implements FileParser 
 {
-    private static int idCounter;
-    public int id;
+    public int id = 0;
     public String name;
     public int weight;
     public boolean conditionUsed;
     public PriceTag priceTag;
     public ProductCategory category;
     public ProductRating rating;
-
-    public Product(int id, int storeId, String name, int weight, boolean conditionUsed, PriceTag priceTag, ProductCategory category)
-    {
+    public int storeId;
+    public Store store;
+    public Shipment.MultiDuration multiDuration;
+ 
+    public Product(int id, int storeId, String name, int weight, boolean conditionUsed, PriceTag priceTag, ProductCategory category, Shipment.MultiDuration multiDuration){
         super(id);
+        this.storeId = storeId;
         this.name = name;
         this.weight = weight;
         this.conditionUsed = conditionUsed;
         this.priceTag = priceTag;
         this.category = category;
-        this.rating = rating;
-        id = idCounter;
-        idCounter++;
+        this.rating = new ProductRating();
+        this.multiDuration = multiDuration;
     }
-
+    
     public String toString(){
-        return(
-            "Name: " + this.name +"\n" +
-            "Weight: " + this.weight + "\n" +
-            "conditionUsed: " + this.conditionUsed + "\n" +
-            "priceTag: " + this.priceTag + "\n" +
-            "category: " + this.category + "\n" +
-            "rating: " + this.rating + "\n" +
-            "storeId: " + this.idCounter + "\n"
-        );
+        return "Name: " + this.name + 
+                "\nWeight: " + this.weight +
+                "\nconditionUsed: " + this.conditionUsed +
+                "\npriceTag: " + this.priceTag +
+                "\ncategory: " + this.category +
+                "\nrating: " + this.rating +
+                "\nstoreId: " + this.storeId;
     }
-
-
+    
+    public boolean read(String content){
+        return false;
+    }
 }
