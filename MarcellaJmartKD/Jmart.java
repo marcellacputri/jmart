831248fd8f7a5228887dec84bd77422cfd739445
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-10-02 10:48:10 +0700

    CS modul4 MarcellaCP

--- MarcellaJmartKD/Jmart.java
+++ MarcellaJmartKD/Jmart.java
@@ -1,37 +1,37 @@
 package MarcellaJmartKD; 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Jmart
 {
-    public static void main (String[] args)
-    {
-        //System.out.println(Shipment.Duration.KARGO.getEstimatedArrival());        
+    public void main (String args[]){
+        System.out.println(Shipment.Duration.KARGO.getEstimatedArrival(new Date())); 
     }
     
-    //public static void main(String[] args){
-        //System.out.println(create());
-    //}
-    
-
     //public static Product create(){
         //return new Product("Dilan", 5, false, new PriceTag(20000L), ProductCategory.BOOK);
     //}
     
-    /*public static void main (String[] args){
-    }
-    
     public static Product createProduct(){
-        return null;
+        return null;   
     }
     
-    public static Coupon createCoupun(){
+    /*public static Coupon createCoupun(){
         Coupon coupon = new Coupon ("Diskon 5.5 SALE", 1, Coupon.Type.DISCOUNT, 20, 5000);
         return coupon;
     }
-    
-    public static Shipment createShipment(){
+    */
+   
+   
+    public static Coupon createCoupon(){
         return null;
-    }*/
+    }
+    
+    public static Shipment.Duration createShipmentDuration(){
+        return null;
+    }
 }
+
         
      
     
