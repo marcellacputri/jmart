4bd136db3fad5f5597b7662f03e2142f0b678aa0
marcellacputri <marcella.cinninthya@ui.ac.id>
2021-11-05 18:52:57 +0700

    TP modul6 Marcella Cinninthya Putri

--- MarcellaJmartKD/Jmart.java
+++ MarcellaJmartKD/Jmart.java
@@ -1,10 +1,36 @@
 package MarcellaJmartKD;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
-public class Jmart {
-	public static void main(String[] args) {
+class Jmart {
+	class Country{
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
+	public static void main (String[] args) {
+		String filepath = "D:/jmart/city.json";
+		Gson gson = new Gson();
+		try {
+			BufferedReader br = new BufferedReader (new FileReader (filepath));
+			Country input = gson.fromJson(br, Country.class);
+			System.out.println("name: " + input.name);
+			System.out.println("population: " + input.population);
+			System.out.println("states: ");
+			input.listOfStates.forEach(state -> System.out.println(state));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+	
+	/*public static void main(String[] args) {
 		System.out.println(Shipment.Duration.INSTANT.getEstimatedArrival(new Date()));
 		Account account = new Account(01, "Spiderman 007", "spiderman123@gmail.com", "mylovelymj");
 		account.validate();
@@ -28,4 +54,3 @@ public class Jmart {
 	 * 
 	 * public static Shipment.Duration createShipmentDuration() { return null; }
 	 */
-}
--- module-info.java
+++
@@ -1,3 +0,0 @@
-module jmart {
-	exports MarcellaJmartKD;
-}
